b7edce9606a908706b62e0c86394b9a52d5a88b3
Rohit Bhattacharya <rohit.bhattachar@gmail.com>
2015-11-18 07:02:53 -0500

    Renaming and commenting
    Renamed and commented retrieve_player_matches.py for league

---
+++ league/retrieve_player_matches.py
@@ -0,0 +1,41 @@
+"""
+Script to fetch player match data
+using the Riot API
+
+authors : Azwad Sabik, Rohit Bhattacharya
+emails  : azwadsabik@gmail.com, rohit.bhattachar@gmail.com
+"""
+
+# imports
+from riotwatcher import RiotWatcher
+import pickle
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# initialize with Riot API key
+with open('Veraxios_API_Key.txt', 'r') as f:
+     lol = RiotWatcher(f.read().strip())
+     
+# name of player to retrieve matches for
+desired_player_name = 'C9 Sneaky'     
+     
+# get dictionary of players in challenger league
+challenger = lol.get_challenger()
+
+# get player objects
+players = challenger['entries']
+
+# iterate through till we find the desired player
+for player in players:
+    if player['playerOrTeamName'] == desired_player_name:
+        desired_player = player
+
+# obtain desired player ID
+desired_player_id = player['playerOrTeamId']
+
+# get a list of matches played ranked solo queue
+# in a particular season
+matches = lol.get_match_list(desired_player_id, ranked_queues='RANKED_SOLO_5x5', 
+                         seasons='SEASON2015')
+                         
